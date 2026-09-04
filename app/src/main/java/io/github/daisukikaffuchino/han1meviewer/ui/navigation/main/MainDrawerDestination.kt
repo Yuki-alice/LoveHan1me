b0f618cca004977c8850c374519a5749c4d5258d
Yuki-alice <1615908977@qq.com>
2026-09-04 13:35:57 +0800

    P6d1-A5: MainDrawerDestination子树2文件（iconRes签名DrawableResource化+MainActivityScaffold联动）

--- app/src/main/java/io/github/daisukikaffuchino/han1meviewer/ui/navigation/main/MainDrawerDestination.kt
+++ app/src/main/java/io/github/daisukikaffuchino/han1meviewer/ui/navigation/main/MainDrawerDestination.kt
@@ -1,58 +1,68 @@
 package io.github.daisukikaffuchino.han1meviewer.ui.navigation.main
 
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import io.github.daisukikaffuchino.han1meviewer.R
+import io.github.daisukikaffuchino.han1meviewer.Res
+import io.github.daisukikaffuchino.han1meviewer.ic_access_time
+import io.github.daisukikaffuchino.han1meviewer.ic_download
+import io.github.daisukikaffuchino.han1meviewer.ic_favorite_border
+import io.github.daisukikaffuchino.han1meviewer.ic_format_list_bulleted
+import io.github.daisukikaffuchino.han1meviewer.ic_history
+import io.github.daisukikaffuchino.han1meviewer.ic_home
+import io.github.daisukikaffuchino.han1meviewer.ic_settings
+import io.github.daisukikaffuchino.han1meviewer.ic_subscribtion
+import io.github.daisukikaffuchino.han1meviewer.ic_thumb_up_off_alt
 import io.github.daisukikaffuchino.han1meviewer.ui.navigation.settings.HomeSettingsRoute
+import org.jetbrains.compose.resources.DrawableResource
 
 enum class MainDrawerDestination(
     val route: HanimeScreen,
-    @param:DrawableRes val iconRes: Int,
+    val iconRes: DrawableResource,
     @param:StringRes val titleRes: Int,
 ) {
     Home(
         route = HomeRoute,
-        iconRes = R.drawable.ic_home,
+        iconRes = Res.drawable.ic_home,
         titleRes = R.string.home_page,
     ),
     Settings(
         route = HomeSettingsRoute,
-        iconRes = R.drawable.ic_settings,
+        iconRes = Res.drawable.ic_settings,
         titleRes = R.string.settings,
     ),
     DailyCheckIn(
         route = DailyCheckInRoute,
-        iconRes = R.drawable.ic_thumb_up_off_alt,
+        iconRes = Res.drawable.ic_thumb_up_off_alt,
         titleRes = R.string.check_in_feature_name,
     ),
     WatchLater(
         route = MyWatchLaterRoute,
-        iconRes = R.drawable.ic_access_time,
+        iconRes = Res.drawable.ic_access_time,
         titleRes = R.string.watch_later,
     ),
     FavVideo(
         route = MyFavVideoRoute,
-        iconRes = R.drawable.ic_favorite_border,
+        iconRes = Res.drawable.ic_favorite_border,
         titleRes = R.string.fav_video,
     ),
     Playlist(
         route = MyPlaylistRoute,
-        iconRes = R.drawable.ic_format_list_bulleted,
+        iconRes = Res.drawable.ic_format_list_bulleted,
         titleRes = R.string.play_list,
     ),
     Subscription(
         route = SubscriptionRoute,
-        iconRes = R.drawable.ic_subscribtion,
+        iconRes = Res.drawable.ic_subscribtion,
         titleRes = R.string.my_subscribe,
     ),
     WatchHistory(
         route = WatchHistoryRoute,
-        iconRes = R.drawable.ic_history,
+        iconRes = Res.drawable.ic_history,
         titleRes = R.string.watch_history,
     ),
     Download(
         route = DownloadRoute,
-        iconRes = R.drawable.ic_download,
+        iconRes = Res.drawable.ic_download,
         titleRes = R.string.download,
     );
 
--- app/src/main/java/io/github/daisukikaffuchino/han1meviewer/ui/screen/main/MainActivityScaffold.kt
+++ app/src/main/java/io/github/daisukikaffuchino/han1meviewer/ui/screen/main/MainActivityScaffold.kt
@@ -41,11 +41,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.res.painterResource
+import org.jetbrains.compose.resources.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.daisukikaffuchino.han1meviewer.R
+import io.github.daisukikaffuchino.han1meviewer.Res
 import io.github.daisukikaffuchino.han1meviewer.ui.navigation.main.MainDrawerDestination
 import io.github.daisukikaffuchino.han1meviewer.ui.preview.ComponentPreview
 import io.github.daisukikaffuchino.han1meviewer.ui.theme.HanimeDefaults
