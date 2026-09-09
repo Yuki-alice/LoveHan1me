2a8a2ee760bbe3ff97f9f42e4af988cdbcd35b1f
Yuki-alice <1615908977@qq.com>
2026-09-09 22:12:46 +0800

    M7: 修复 iOS 模拟器启动崩溃, 完成模拟器端到端验证
    - Info.plist 添加 CADisableMinimumFrameDurationOnPhone=true,
      满足 Compose PlistSanityCheck, 修复启动即 SIGABRT
    - LogUtil.ios.kt 显式转换 NSString 再传入 NSLog,
      规避 K/N variadic 参数不做 String->NSString 桥接导致的 SIGSEGV
    
    验证: xcodebuild -target 构建成功, 模拟器运行稳定,
    首页网络请求 200 且解析正常, 无崩溃

--- shared/src/iosMain/kotlin/io/github/daisukikaffuchino/utils/LogUtil.ios.kt
+++ shared/src/iosMain/kotlin/io/github/daisukikaffuchino/utils/LogUtil.ios.kt
@@ -1,6 +1,8 @@
 package io.github.daisukikaffuchino.utils
 
 import platform.Foundation.NSLog
+import platform.Foundation.NSString
+import platform.Foundation.create
 
 // iOS：NSLog；异常栈用 stackTraceToString() 追加（Native 没有 printStackTrace 的等价输出通道）
 internal actual fun platformLog(
@@ -10,5 +12,7 @@ internal actual fun platformLog(
     throwable: Throwable?,
 ) {
     val text = if (throwable == null) message else "$message\n${throwable.stackTraceToString()}"
-    NSLog("%@", "[$tag]$text")
+    // 必须显式转成 NSString 再传入：NSLog 是 variadic 函数，K/N 对 variadic 参数
+    // 不会自动做 String→NSString 桥接，直接传 Kotlin String 会被 ObjC 端当作坏指针导致 SIGSEGV。
+    NSLog("%@", NSString.create(string = "[$tag]$text"))
 }
